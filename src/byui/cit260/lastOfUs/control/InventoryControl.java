6ecf348d90c89f6c371d36fe0ea1eaa1c5d04de4
AmaroIdelfonso <AmaroIdelfonso@Amaro-pc>
2017-02-09 22:01:21 -0300

    Inclusão de control Inventory class e InventoryTest.

---
+++ src/byui/cit260/lastOfUs/control/InventoryControl.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.lastOfUs.control;
+
+/**
+ *
+ * @author Survivors of Brazil
+ */
+public class InventoryControl {
+    
+    public double calcBottlerOfWather(int distance, double consume){
+        if (distance >= 48){
+            return -1;
+        }
+        if (consume != 0.055){
+            return -1;
+        }
+        
+    double liters = (consume * distance);
+    return liters;
+    }
+    
+}

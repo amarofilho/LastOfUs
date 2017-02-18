8fd7f7d32590a9aec0acacffbdbfed0a4e8afe9e
AmaroIdelfonso <AmaroIdelfonso@Amaro-pc>
2017-02-18 12:53:50 -0300

    Atualização da View Class. By Amaro 18/02/2017

--- src/byui/cit260/lastOfUs/view/StartProgramView.java
+++ src/byui/cit260/lastOfUs/view/StartProgramView.java
@@ -97,7 +97,15 @@ public class StartProgramView {
     }
 
     private void displayNextView(Player player) {
-        System.out.println("\n DisplayNextView called");
+        System.out.println("\n=================================================="
+                          +"\n Welcome to the game " + player.getName()
+                          +"\n We hope you have a lot of fun!"
+                          +"\n=================================================="
+                          );
+        MainMenuView mainMenuView = new MainMenuView();
+                
+        mainMenuView.displayMainMenuView();        
+            
     }
     
 }

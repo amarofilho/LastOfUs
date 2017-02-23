789e75493d13386feddd66c10f9e88e8c9dcedf9
AmaroIdelfonso <AmaroIdelfonso@Amaro-pc>
2017-02-22 21:43:03 -0300

    parte dois excercicio 1 By Amaro 22/02/2017

---
+++ src/byui/cit260/lastOfUs/view/CalcWaterView.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.lastOfUs.view;
+
+import byui.cit260.lastOfUs.control.InventoryControl;
+
+/**
+ *
+ * @author AmaroIdelfonso
+ */
+public class CalcWaterView {
+    void calcWater(){
+      InventoryControl calcBottlerOfWather = new InventoryControl();
+      calcBottlerOfWather.displayInventoryControl(int distance,double consume);
+        
+    }
+}
